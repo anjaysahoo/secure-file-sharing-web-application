5d487b0e8e14e8bee08e294eba76ed48e2ba8c4b
Anjay Sahoo <65654701+anjaysahoo@users.noreply.github.com>
2024-12-16 21:14:59 +0530

    🔨bug: fixed filename download issue

--- frontend/src/components/FileActions.tsx
+++ frontend/src/components/FileActions.tsx
@@ -30,18 +30,14 @@ const FileActions: React.FC<FileActionsProps> = ({ fileId, fileName, onDelete, o
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement('a');
             link.href = url;
-            const contentDisposition = response.headers['content-disposition'];
-            if (contentDisposition && contentDisposition.includes('filename=')) {
-                const filename = contentDisposition.split('filename=')[1].replace(/"/g, '');
-                link.setAttribute('download', filename);
-            } else {
-                link.setAttribute('download', `file_${fileId}`);
-            }
+            link.setAttribute('download', fileName);
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
         } catch (error) {
             console.error('Failed to download file:', error);
+            onMessage('Failed to download file');
         }
     };
 
